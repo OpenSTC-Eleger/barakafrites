ce60a3318a3fc937242c8cd496d6de14269ad0d2
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-07-02 17:27:29 +0200

    refs #825 Fixes openobject model name to singular

--- app/models/openstc/department.rb
+++ app/models/openstc/department.rb
@@ -1,6 +1,6 @@
 class Openstc::Department
   include OpenObjectModel
-  set_open_object_model 'openstc.services'
+  set_open_object_model 'openstc.service'
 
   @@available_fields = %w(id href name)
   attr_accessor *@@available_fields
