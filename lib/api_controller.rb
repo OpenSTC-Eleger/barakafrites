a685da5baa7c2b8f71c6fb4e85e365d77c556061
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-07-02 12:03:26 +0200

    fix fields params parsing

--- lib/api_controller.rb
+++ lib/api_controller.rb
@@ -6,7 +6,7 @@ module ApiController
   def format_filters(params_filter)
     filters = if params_filter.size > 0
                 params_filter.map do |filter|
-                  [filter[:field], filter[:operator], filter[:value]]
+                  [filter['field'], filter['operator'], filter['value']]
                 end
               else
                 params_filter
