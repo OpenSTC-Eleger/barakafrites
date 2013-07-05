9d9c18ab602fd506e4a76ba2d5c8113cbe8e0445
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-07-05 10:38:57 +0200

    fixes #1063 Intervention_request spec tests fails :  Refactored Controller and Specs

--- app/controllers/api/openstc/intervention_requests_controller.rb
+++ app/controllers/api/openstc/intervention_requests_controller.rb
@@ -1,31 +1,6 @@
-class Api::Openstc::InterventionRequestsController < ApplicationController
+class Api::Openstc::InterventionRequestsController < Api::ResourceController
+  include ApiController
 
-  before_filter :check_authenticated?
-
-
-  def index
-    @filters = params[:filters] || []
-    @fields = params[:fields] || []
-    @intervention_requests = Openstc::InterventionRequest.find_all(user_context, format_filters(@filters), @fields)
-    backend_response_to_json @intervention_requests
-  end
-
-  def show
-    @intervention_request = Openstc::InterventionRequest.find_one(user_context, params[:id])
-    backend_response_to_json @intervention_request
-  end
-
-  def update
-    @update = Openstc::InterventionRequest.write(user_context, [params[:id]], params[:intervention_request])
-    backend_response_to_json @update
-  end
-
-  ##
-  # TODO : Refactor to avoid code duplication
-  #
-  def create
-    @create = Openstc::InterventionRequest.create_and_return(user_context, params[:intervention_request])
-    backend_response_to_json @create
-  end
+  self.resource_model = ::Openstc::InterventionRequest
 
 end
