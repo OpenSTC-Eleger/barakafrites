f1af28d55ae2726037b6d6bb9935f04687c86862
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-07-18 11:11:51 +0200

    Re enabled Hash With indiferent access for config

--- config/initializers/bintje_initializer.rb
+++ config/initializers/bintje_initializer.rb
@@ -1,10 +1,10 @@
 module OpenObject
 
-  @@host = BarakafritesConfig.get[:open_object][:host]
-  @@port = BarakafritesConfig.get[:open_object][:port]
-  @@common = BarakafritesConfig.get[:open_object][:common]
-  @@object = BarakafritesConfig.get[:open_object][:object]
-  @@base = BarakafritesConfig.get[:open_object][:base]
+  @@host = BarakafritesConfig.get['open_object']['host']
+  @@port = BarakafritesConfig.get['open_object']['port']
+  @@common = BarakafritesConfig.get['open_object']['common']
+  @@object = BarakafritesConfig.get['open_object']['object']
+  @@base = BarakafritesConfig.get['open_object']['base']
 
 end
 
