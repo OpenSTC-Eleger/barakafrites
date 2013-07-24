fab754ff3ab8744789780495e0a5942e3f50aafe
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-07-24 14:40:04 +0200

    refs #1202 fixes wrong model name and fields

--- app/models/openstc/intervention_category.rb
+++ app/models/openstc/intervention_category.rb
@@ -1,5 +1,9 @@
 class Openstc::InterventionCategory
   include OpenObjectModel
+  set_open_object_model 'openstc.intervention.assignement'
 
+  @@available_fields = %w(code id name)
+
+  attr_accessor *@@available_fields
 
 end
--- app/models/openstc/intervention_request.rb
+++ app/models/openstc/intervention_request.rb
@@ -2,8 +2,9 @@ class Openstc::InterventionRequest
   include OpenObjectModel
   set_open_object_model 'openstc.ask'
   
-  @@available_fields = %w(site_details create_date partner_type_code write_uid refusal_reason partner_id id create_uid intervention_assignement_id people_name note current_date manager_id confirm_by_dst people_phone partner_address partner_email description state people_email partner_type actions name partner_service_id site2 site3 site1 partner_phone service_id intervention_ids date_deadline tooltip)
-  
+  @@available_fields = %w(actions belongsToAssignement tooltip belongsToService id name belongsToSite create_date create_uid date_deadline description id intervention_assignement_id intervention_ids manager_id name note partner_address partner_email partner_id partner_phone partner_service_id partner_type partner_type_code people_email people_name people_phone refusal_reason service_id site1 site_details state write_uid)
+
+
   attr_accessor *@@available_fields
   
 
