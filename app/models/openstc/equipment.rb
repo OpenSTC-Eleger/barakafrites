8d48dd441fff1f6785d4412f16ddd2720b86d076
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-08-06 16:37:40 +0200

    fixes Intervention & Equipment openstc models

--- app/models/openstc/equipment.rb
+++ app/models/openstc/equipment.rb
@@ -1,6 +1,7 @@
 class Openstc::Equipment
   include OpenObjectModel
-  set_open_object_model = 'openstc.equipment'
+
+  set_open_object_model  'openstc.equipment'
   @@available_fields = %w( id name href )
   attr_accessor *@@available_fields
 
--- app/models/openstc/intervention.rb
+++ app/models/openstc/intervention.rb
@@ -1,3 +1,4 @@
 class Openstc::Intervention
   include OpenObjectModel
+  set_open_object_model 'project.project'
 end
