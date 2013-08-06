48f9ce6faff3a838af7f8e5fcb1782345c8dd088
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-08-06 16:41:31 +0200

    fixes Intervention fields

--- app/models/openstc/intervention.rb
+++ app/models/openstc/intervention.rb
@@ -1,4 +1,5 @@
 class Openstc::Intervention
   include OpenObjectModel
   set_open_object_model 'project.project'
+  @@available_fields = %w(id name description state tasks service_id site1 date_start date_end)
 end
