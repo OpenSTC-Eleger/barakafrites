219bbd08c46d50bda899deccb77075d28a87758a
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-08-06 16:28:09 +0200

    fixes typo added service name to openstc:site property

--- app/models/openstc/site.rb
+++ app/models/openstc/site.rb
@@ -2,7 +2,7 @@ class Openstc::Site
   include OpenObjectModel
   set_open_object_model 'openstc.site'
 
-  @@available_fields = %w(code complete_name id lenght name service_name service_ids site_parent_id surface type width href)
+  @@available_fields = %w(complete_name id lenght name service_names service_ids site_parent_id surface type width href)
 
   @@available_fields.each do |field|
     attr_accessor field
