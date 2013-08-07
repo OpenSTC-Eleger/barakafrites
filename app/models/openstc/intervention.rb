5910d98f4d634906512a60fa57545a5e1871c737
plancher bruno <bruno.plancher@gmail.com>
2013-08-07 10:06:56 +0200

    completed available_fields of Interventions

--- app/models/openstc/intervention.rb
+++ app/models/openstc/intervention.rb
@@ -1,7 +1,7 @@
 class Openstc::Intervention
   include OpenObjectModel
   set_open_object_model 'project.project'
-  @@available_fields = %w(id name description tasks state service_id site1 date_start date_end actions)
+  @@available_fields = %w(id name description tasks state service_id site1 date_deadline planned_hours effective_hours tooltip progress_rate overPourcent actions)
 
   @@available_fields.each do |field|
     attr_accessor field
