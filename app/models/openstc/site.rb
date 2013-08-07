11f0815793035741608389f0dcb53505dbdd52f4
Simon <simon.mahe@siclic.fr>
2013-08-07 12:29:12 +0200

    Ajout du champ actions pour les sites

--- app/models/openstc/site.rb
+++ app/models/openstc/site.rb
@@ -2,7 +2,7 @@ class Openstc::Site
   include OpenObjectModel
   set_open_object_model 'openstc.site'
 
-  @@available_fields = %w(complete_name id length name service_names service_ids site_parent_id surface type width href)
+  @@available_fields = %w(complete_name id length name service_names service_ids site_parent_id surface type width href actions)
 
   @@available_fields.each do |field|
     attr_accessor field
