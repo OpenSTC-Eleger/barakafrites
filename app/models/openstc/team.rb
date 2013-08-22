6f6dcb7df067d67f8d783b41f984ca6f461cce68
Simon <simon.mahe@siclic.fr>
2013-08-22 11:16:36 +0200

    Ajout des champs service_names user_names pour le modele Team

--- app/models/openstc/team.rb
+++ app/models/openstc/team.rb
@@ -3,7 +3,7 @@ class Openstc::Team
 
   set_open_object_model 'openstc.team'
 
-  @@available_fields = %w(id href name manager_id actions service_ids user_ids)
+  @@available_fields = %w(id href name manager_id actions service_ids user_ids service_names user_names)
   attr_accessor *@@available_fields
 
 end
