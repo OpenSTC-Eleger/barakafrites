2fc754982bdff5eeb9b20eb1bacdc4709cb00722
Simon <simon.mahe@siclic.fr>
2013-08-23 14:22:01 +0200

    Ajout du champ actions pour le modèle catégorie des demandes d'interventions

--- app/models/openstc/intervention_category.rb
+++ app/models/openstc/intervention_category.rb
@@ -2,7 +2,7 @@ class Openstc::InterventionCategory
   include OpenObjectModel
   set_open_object_model 'openstc.intervention.assignement'
 
-  @@available_fields = %w(code id name)
+  @@available_fields = %w(code id name actions)
 
   attr_accessor *@@available_fields
 
