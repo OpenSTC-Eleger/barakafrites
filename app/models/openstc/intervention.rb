d8217042615ff48f893894a608f07d4853dff747
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-08-26 17:21:30 +0200

    Added totalhours field in interventions

--- app/models/openstc/intervention.rb
+++ app/models/openstc/intervention.rb
@@ -1,7 +1,7 @@
 class Openstc::Intervention
   include OpenObjectModel
   set_open_object_model 'project.project'
-  @@available_fields = %w(id name description tasks state service_id site1 date_deadline planned_hours effective_hours tooltip progress_rate overPourcent actions ask_id create_uid)
+  @@available_fields = %w(id name description tasks state service_id site1 date_deadline planned_hours effective_hours tooltip progress_rate overPourcent actions ask_id create_uid total_hours)
 
   @@available_fields.each do |field|
     attr_accessor field
