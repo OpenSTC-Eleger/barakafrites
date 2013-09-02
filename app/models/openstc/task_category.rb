969b84c7a0b9bb1280e56080669cb466e1c5367d
Simon <simon.mahe@siclic.fr>
2013-09-02 16:11:42 +0200

    Ajout du champ service_names pour le model catégorie de tâche

--- app/models/openstc/task_category.rb
+++ app/models/openstc/task_category.rb
@@ -3,7 +3,7 @@ class Openstc::TaskCategory
 
   set_open_object_model 'openstc.task.category'
 
-  @@available_fields = %w(code complete_name id name parent_id service_ids actions)
+  @@available_fields = %w(code complete_name id name parent_id service_ids service_names actions)
   attr_accessor *@@available_fields
 
 end
