4635502c532ab6e817fec8680c314df378440ee9
Simon <simon.mahe@siclic.fr>
2013-09-02 15:01:02 +0200

    Ajout du champ action pour les catégories de tâches

--- app/models/openstc/task_category.rb
+++ app/models/openstc/task_category.rb
@@ -3,7 +3,7 @@ class Openstc::TaskCategory
 
   set_open_object_model 'openstc.task.category'
 
-  @@available_fields = %w(code complete_name id name parent_id service_ids)
+  @@available_fields = %w(code complete_name id name parent_id service_ids actions)
   attr_accessor *@@available_fields
 
 end
