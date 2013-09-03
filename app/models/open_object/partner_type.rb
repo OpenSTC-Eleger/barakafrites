dcfe8b13dd806205dae7ca220b452573187f5800
Simon <simon.mahe@siclic.fr>
2013-09-03 15:43:35 +0200

    Ajout du champ action pour le modèle partner_type

--- app/models/open_object/partner_type.rb
+++ app/models/open_object/partner_type.rb
@@ -3,7 +3,7 @@ class OpenObject::PartnerType
 
   set_open_object_model 'openstc.partner.type'
 
-  @@available_fields = %w(claimers code id name)
+  @@available_fields = %w(claimers code id name actions)
   attr_accessor *@@available_fields
 
 end
