28c37ca2b5632844a5aa43784c8db3ae62f7c1f7
Simon <simon.mahe@siclic.fr>
2013-09-03 11:17:40 +0200

    Ajout des champs pour le modèle Absent Type

--- app/models/openstc/absence_category.rb
+++ app/models/openstc/absence_category.rb
@@ -1,7 +1,8 @@
 class Openstc::AbsenceCategory
   include OpenObjectModel
+  set_open_object_model 'openstc.absent.type'
 
-  @@available_fields = %w( id name href )
+  @@available_fields = %w( id name href code description actions)
   attr_accessor *@@available_fields
 
 end
