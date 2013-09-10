ff25a34c3ebf76a832f0a1b566679ff92288cd8e
plancher bruno <bruno.plancher@gmail.com>
2013-09-10 10:18:15 +0200

    added fields on request_interventions (has_equipment, equipemnt_ids) and on equipment (internal_use, maintenance_service_ids)

--- app/models/openstc/equipment.rb
+++ app/models/openstc/equipment.rb
@@ -2,7 +2,7 @@ class Openstc::Equipment
   include OpenObjectModel
 
   set_open_object_model  'openstc.equipment'
-  @@available_fields = %w( id name href )
+  @@available_fields = %w( id name href maintenance_service_ids internal_use)
   attr_accessor *@@available_fields
 
 end
--- app/models/openstc/intervention_request.rb
+++ app/models/openstc/intervention_request.rb
@@ -2,7 +2,7 @@ class Openstc::InterventionRequest
   include OpenObjectModel
   set_open_object_model 'openstc.ask'
   
-  @@available_fields = %w(actions belongsToAssignement tooltip belongsToService id name belongsToSite create_date create_uid date_deadline description id intervention_assignement_id intervention_ids manager_id name note partner_address partner_email partner_id partner_phone partner_service_id partner_type partner_type_code people_email people_name people_phone refusal_reason service_id site1 site_details state write_uid)
+  @@available_fields = %w(actions belongsToAssignement tooltip belongsToService id name belongsToSite create_date create_uid date_deadline description id intervention_assignement_id intervention_ids manager_id name note partner_address partner_email partner_id partner_phone partner_service_id partner_type partner_type_code people_email people_name people_phone refusal_reason service_id site1 site_details state write_uid has_equipment equipment_id)
 
 
   attr_accessor *@@available_fields
