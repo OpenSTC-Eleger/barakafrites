75ef1a593e5f26e0ff51132e620356b6368c7b17
plancher bruno <bruno.plancher@gmail.com>
2013-09-13 16:07:31 +0200

    added fields to EquipmentCategories: is_vehicle, is_equipment, parent_id

--- app/models/openstc/equipment_categories.rb
+++ app/models/openstc/equipment_categories.rb
@@ -2,7 +2,7 @@ class Openstc::EquipmentCategories
   include OpenObjectModel
 
   set_open_object_model  'product.category'
-  @@available_fields = %w( id name href)
+  @@available_fields = %w( id name href parent_id is_vehicle is_equipment)
   attr_accessor *@@available_fields
 
 end
