d6e308f7dc8e36a9a49b4eef95927f6b87865754
pyfadet <pierreyves.fadet@siclic.fr>
2013-11-08 16:51:30 +0100

    booking reccurent

--- app/models/openresa/booking.rb
+++ app/models/openresa/booking.rb
@@ -2,7 +2,7 @@ class Openresa::Booking
   include OpenObjectModel
   set_open_object_model 'hotel.reservation'
 
-  @@available_fields = %w( id name href prod_id checkin checkout partner_id create_date state actions reservation_line )
+  @@available_fields = %w( id name href prod_id checkin checkout partner_id create_date state state_num actions reservation_line create_uid resource_names resource_quantities all_dispo recurrence_id is_template )
   attr_accessor *@@available_fields
 
 end
