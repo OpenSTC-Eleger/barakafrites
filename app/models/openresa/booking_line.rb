7e566a00268e9870ef2c15a429b19d04fc595b9e
plancher bruno <bruno.plancher@gmail.com>
2013-11-25 10:32:05 +0100

    added field 'dispo' on booking_line model

--- app/models/openresa/booking_line.rb
+++ app/models/openresa/booking_line.rb
@@ -2,7 +2,7 @@ class Openresa::BookingLine
   include OpenObjectModel
   set_open_object_model 'hotel_reservation.line'
 
-  @@available_fields = %w( id name href reserve_product qte_dispo qte_reserves pricelist_amount)
+  @@available_fields = %w( id name href reserve_product qte_dispo qte_reserves pricelist_amount dispo)
   attr_accessor *@@available_fields
 
 end
