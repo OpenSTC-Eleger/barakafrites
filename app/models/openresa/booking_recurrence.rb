1fc537f7515b45ca5d2c7b12ecffe9e1740ec26c
pyfadet <pierreyves.fadet@siclic.fr>
2013-11-28 17:42:29 +0100

    actions de validation refus, clôture dans la liste des réservations

--- app/models/openresa/booking_recurrence.rb
+++ app/models/openresa/booking_recurrence.rb
@@ -2,7 +2,7 @@ class Openresa::BookingRecurrence
   include OpenObjectModel
   set_open_object_model 'openresa.reservation.recurrence'
 
-  @@available_fields = %w( id name href )
+  @@available_fields = %w( id name actions )
   attr_accessor *@@available_fields
 
 
