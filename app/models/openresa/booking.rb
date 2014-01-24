1fada37cf33bbc64c7e796840c0ef8b42f4bb8bf
plancher bruno <bruno.plancher@gmail.com>
2014-01-24 13:52:26 +0100

    added field amount_total to booking resource

--- app/models/openresa/booking.rb
+++ app/models/openresa/booking.rb
@@ -3,7 +3,7 @@ class Openresa::Booking
   set_open_object_model 'hotel.reservation'
 
 
-  @@available_fields = %w( id name href prod_id checkin checkout partner_id partner_order_id partner_type contact_phone partner_mail people_name people_email people_phone is_citizen create_date write_date deleted_at confirm_at done_at cancel_at state state_num actions reservation_line create_uid write_uid all_dispo recurrence_id is_template partner_invoice_id note confirm_note cancel_note done_note pricelist_id state_event resource_ids resources people_street people_city people_zip whole_day invoice_attachment_id)
+  @@available_fields = %w( id name href prod_id checkin checkout partner_id partner_order_id partner_type contact_phone partner_mail people_name people_email people_phone is_citizen create_date write_date deleted_at confirm_at done_at cancel_at state state_num actions reservation_line create_uid write_uid all_dispo recurrence_id is_template partner_invoice_id note confirm_note cancel_note done_note pricelist_id state_event resource_ids resources people_street people_city people_zip whole_day invoice_attachment_id amount_total)
 
   attr_accessor *@@available_fields
 
