740787c2c7d7d65c2a933f8a2cab620ae105017d
pyfadet <pierreyves.fadet@siclic.fr>
2014-02-13 14:39:40 +0100

    refs #2456 : Ajout sur modèle filter champs description/pre_recorded

--- app/models/open_object/filter.rb
+++ app/models/open_object/filter.rb
@@ -4,7 +4,7 @@ class OpenObject::Filter
   set_open_object_model 'ir.filters'
 
 
-  @@available_fields = %w( id name href user_id domain context model_id )
+  @@available_fields = %w( id name href user_id domain context model_id description pre_recorded)
   attr_accessor *@@available_fields
 
 
