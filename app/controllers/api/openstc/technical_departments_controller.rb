77e7e09493332f50212f225bf05f7c1ffbe8b940
plancher bruno <bruno.plancher@gmail.com>
2014-03-26 12:03:44 +0100

    added resource 'technical_department' which only fetch department with technical=True

---
+++ app/controllers/api/openstc/technical_departments_controller.rb
@@ -0,0 +1,33 @@
+##
+#
+# Barakafrites helps openerp 6 to speak REST
+#
+#    Copyright (C) 2013  Siclic
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##
+
+
+class Api::Openstc::TechnicalDepartmentsController < Api::Openstc::DepartmentsController
+  include Api::ApiControllerModule
+  self.resource_model=(::Openstc::Department)
+  
+  def index
+    params[:filters] = params[:filters] || {}
+    params[:filters]['is_technical'] = {field: 'technical', operator: '=', value: true}
+  super
+  end
+    
+end
