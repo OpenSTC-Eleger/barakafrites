56d18f834f4d059650676e50c090c0fc70802233
plancher bruno <bruno.plancher@gmail.com>
2014-04-01 16:39:11 +0200

    added openstc/task_recurrences model and controller

---
+++ app/controllers/api/openstc/task_recurrences_controller.rb
@@ -0,0 +1,30 @@
+##
+#
+# Barakafrites helps openerp 6 to speak REST
+#
+#    Copyright (C) 2013  Siclic
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##
+
+
+class Api::Openstc::TaskRecurrencesController < Api::ResourceController
+  resource_description do
+    short 'Specific URIs to interact with/from tasks'
+  end
+  include Api::ApiControllerModule
+  self.resource_model=(::Openstc::TaskRecurrence)
+
+end
---
+++ app/models/openstc/task_recurrence.rb
@@ -0,0 +1,32 @@
+##
+#
+# Barakafrites helps openerp 6 to speak REST
+#
+#    Copyright (C) 2013  Siclic
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##
+
+
+class Openstc::TaskRecurrence
+  include OpenObjectModel
+
+  set_open_object_model 'openstc.task.recurrence'
+
+  @@available_fields = %w( id name href is_team agent_id team_id internal_inter technical_service_id planned_hours task_categ_id supplier_cost recur_periodicity recur_week_monday recur_week_tuesday recur_week_wednesday recur_week_thursday recur_week_friday recur_week_saturday recur_week_sunday recur_month_type recur_month_absolute recur_month_relative_weight recur_month_relative_day recur_type date_start date_end recur_occurrence_nb occurrence_ids recurrence)
+  attr_accessor *@@available_fields
+
+end
+
