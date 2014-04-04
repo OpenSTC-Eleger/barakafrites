05e097e8a61f93727452322918524f2c2293b897
plancher bruno <bruno.plancher@gmail.com>
2014-04-04 15:42:33 +0200

    added field 'contract_id' to openstc/interventions

--- app/models/openstc/intervention.rb
+++ app/models/openstc/intervention.rb
@@ -24,7 +24,7 @@ class Openstc::Intervention
   include OpenObjectModel
   set_open_object_model 'project.project'
 
-  @@available_fields = %w(id name description tasks state service_id site1 site_details date_deadline planned_hours effective_hours tooltip progress_rate overPourcent actions ask_id create_uid create_date total_hours has_equipment equipment_id todo_tasks)
+  @@available_fields = %w(id name description tasks state service_id site1 site_details date_deadline planned_hours effective_hours tooltip progress_rate overPourcent actions ask_id create_uid create_date total_hours has_equipment equipment_id todo_tasks contract_id)
 
   @@available_fields.each do |field|
     attr_accessor field
