4ad152cc053a1eee364e709422b8857013e46bb7
plancher bruno <bruno.plancher@gmail.com>
2014-06-02 12:46:54 +0200

    [resources:purchase] added field 'reception_progress'

--- app/models/open_achats_stock/purchase.rb
+++ app/models/open_achats_stock/purchase.rb
@@ -24,7 +24,7 @@ class OpenAchatsStock::Purchase
   include OpenObjectModel
   set_open_object_model 'purchase.order'
 
-  @@available_fields = %w( id name href date_order description service_id partner_id amount_untaxed amount_tax amount_total state validation validation_order actions check_dst check_elu user_id attach_invoices attach_not_invoices attach_waiting_invoice_ids engage_to_treat account_analytic_id order_line)
+  @@available_fields = %w( id name href date_order description service_id partner_id amount_untaxed amount_tax amount_total state validation validation_order actions check_dst check_elu user_id attach_invoices attach_not_invoices attach_waiting_invoice_ids engage_to_treat account_analytic_id order_line reception_progress)
 
   @@related_fields = {"service_id" => "Openstc::Department", "partner_id" => "OpenObject::Supplier", "account_analytic_id" => "OpenAchatsStock::BudgetLine"}
 
